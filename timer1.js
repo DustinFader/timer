930e98fb1569d7a55039ad6d5a36590aa5806da9
DustinFader <dustinfader16@gmail.com>
2023-10-14 09:33:43 -0300

    minor fixes

--- timer1.js
+++ timer1.js
@@ -1,18 +1,16 @@
-const { builtinModules } = require("module");
-
 // node timer1.js 10 3 5 -1 y NaN undefined [1] null true false " "
 const timer = (alarms) => {
   for (let i = 0; i < alarms.length; i++) {
     // checks if current item from alarms is a number and its positive
     // side effect that " " gets turned to 0 by numbers
     if (Number(alarms[i]) >= 0 && alarms[i].trim() !== "") {
-    setTimeout(() => {
+      setTimeout(() => {
         process.stdout.write('\x07'); // makes a bell sound
         process.stdout.write('.\n');
       }, alarms[i] * 1000);
     }
   }
-}
+};
 
 //let args = process.argv.splice(2);
 //timer(args);
--- timer2.js
+++ timer2.js
@@ -11,11 +11,11 @@ rl.on('line', (input) => { // input
   if (input === "b") { // if b then immediate beep
     process.stdout.write('\x07');
     process.stdout.write('.\n');
-  };
+  }
   timer(input);
 });
 
 rl.on('SIGINT', () => { // ctr + c exit
-  console.log("Thanks for using me, ciao!")
+  console.log("Thanks for using me, ciao!");
   rl.close();
 });
