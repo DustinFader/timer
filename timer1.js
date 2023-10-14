eebf116e8ad5005abfb20cb2a941495994e7c20a
DustinFader <dustinfader16@gmail.com>
2023-10-14 09:26:58 -0300

    now check for both '' and ' '

--- timer1.js
+++ timer1.js
@@ -5,7 +5,7 @@ const timer = (alarms) => {
   for (let i = 0; i < alarms.length; i++) {
     // checks if current item from alarms is a number and its positive
     // side effect that " " gets turned to 0 by numbers
-    if (Number(alarms[i]) >= 0 && alarms[i] !== " ") {
+    if (Number(alarms[i]) >= 0 && alarms[i].trim() !== "") {
     setTimeout(() => {
         process.stdout.write('\x07'); // makes a bell sound
         process.stdout.write('.\n');
@@ -14,7 +14,7 @@ const timer = (alarms) => {
   }
 }
 
-//let args = process.argv.splice(2);
-//timer(args);
+let args = process.argv.splice(2);
+timer(args);
 
 module.exports = timer;
