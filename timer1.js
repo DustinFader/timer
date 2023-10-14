be5b4fc682648abb1a41cc139d83fbeb7ded8ba4
DustinFader <dustinfader16@gmail.com>
2023-10-14 09:20:38 -0300

    made a timer that takes in an input from the terminal and sets timers based on input

--- timer1.js
+++ timer1.js
@@ -1,9 +1,12 @@
-// node timer1.js 10 3 5 -1 y NaN undefined [1] null true false
+const { builtinModules } = require("module");
+
+// node timer1.js 10 3 5 -1 y NaN undefined [1] null true false " "
 const timer = (alarms) => {
   for (let i = 0; i < alarms.length; i++) {
     // checks if current item from alarms is a number and its positive
-    if (Number(alarms[i]) >= 0) {
-      setTimeout(() => {
+    // side effect that " " gets turned to 0 by numbers
+    if (Number(alarms[i]) >= 0 && alarms[i] !== " ") {
+    setTimeout(() => {
         process.stdout.write('\x07'); // makes a bell sound
         process.stdout.write('.\n');
       }, alarms[i] * 1000);
@@ -11,5 +14,7 @@ const timer = (alarms) => {
   }
 }
 
-let args = process.argv.splice(2);
-timer(args);
+//let args = process.argv.splice(2);
+//timer(args);
+
+module.exports = timer;
---
+++ timer2.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+const timer = require('./timer1');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+console.log("Input");
+rl.on('line', (input) => { // input
+  if (input === "b") { // if b then immediate beep
+    process.stdout.write('\x07');
+    process.stdout.write('.\n');
+  };
+  timer(input);
+});
+
+rl.on('SIGINT', () => { // ctr + c exit
+  console.log("Thanks for using me, ciao!")
+  rl.close();
+});
